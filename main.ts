bccdf43ed129294bed012ffff492ac1f7efa9b29
Eduard Filatov <edik93582@gmail.com>
2025-03-11 22:06:15 +0300

    feat: add first simple form

--- main.ts
+++ main.ts
@@ -1,134 +1,144 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, ItemView, WorkspaceLeaf } from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.registerView("finance-view", (leaf) => new FinancePluginView(leaf));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.addRibbonIcon("dollar-sign", "Open Finance Panel", () => {
+      this.activateView();
+    });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText("Status Bar Text");
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: "open-sample-modal-simple",
+      name: "Open sample modal (simple)",
+      callback: () => {
+        new SampleModal(this.app).open();
+      },
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async activateView() {
+    const { workspace } = this.app;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // Проверяем, открыта ли уже панель
+    let leaf = workspace.getLeavesOfType("finance-view")[0];
 
-	onunload() {
+    if (!leaf) {
+      // Если панель не открыта, создаём новую
+      leaf = workspace.getRightLeaf(false);
+      await leaf.setViewState({ type: "finance-view" });
+    }
 
-	}
+    // Активируем панель
+    workspace.revealLeaf(leaf);
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  onunload() {}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
+
+class FinancePluginView extends ItemView {
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
+
+  private amountPerDay: number = 0;
+  private today: string = new Date().toISOString().split("T")[0];
+  private untilDate: string; // дата до какого числа у нас сумму;
+
+  getViewType(): string {
+    return "finance-view"; // Возвращаем уникальный идентификатор
+  }
+
+  getDisplayText(): string {
+    return "Finance Panel"; // Название панели
+  }
+
+  getIcon(): string {
+    return "dollar-sign"; // Иконка для панели
+  }
+
+  private changeInputHandler(el: HTMLInputElement) {
+    el.addEventListener("input", (evt: Event) => {
+      console.log(evt);
+      // Добавляем обработчик события input
+      // const value = (evt.target as HTMLInputElement).value;
+    });
+  }
+
+  private setUntilDate(el: HTMLInputElement) {
+    el.addEventListener("change", (evt: Event) => {
+      this.untilDate = (evt.target as HTMLInputElement).value;
+      console.log();
+    });
+  }
+
+  async onOpen() {
+    const container = this.containerEl.children[1];
+    container.empty();
+    container.createEl("h2", { text: "Welcome to Finance Plugin!" });
+
+    container.createEl("p", { text: "Введите сумму:" });
+    container.createEl("input", undefined, (el) => this.changeInputHandler(el));
+
+    container.createEl("p", { text: "Срок (до какого числа):" });
+    container.createEl("input", { type: "date" }, (el) =>
+      this.setUntilDate(el)
+    );
+
+    container.createEl("p", {
+      text: `Срок (до какого числа):${this.amountPerDay}`,
+    });
+  }
+
+  async onClose() {
+    // Очистка ресурсов, если нужно
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
